0eafd49c49ed76cea4e60f17f3e07fb2c3504b5c
LurkerWithScarf <cursedseraph@mail.ru>
2019-12-04 22:26:35 +0300

    Added a new script.

---
+++ 2.cpp
@@ -0,0 +1,390 @@
+#include <iostream>
+#include <experimental/filesystem>
+#include <set>
+#include <map>
+#include <list>
+#include <fstream>
+#include <iomanip>
+
+// TODO solve error with the following problem
+// when you ignore a folder, you mustn't visit it, but recursive iterator goes inside it.
+// Tip. Make it bfs like(layer by layer).
+
+enum class File
+{
+    IGNORE = 0,
+    UNKNOWN = 1,
+    ADD = 2
+};
+
+int files = 0;
+int unknown_files = 0;
+int ignored_files = 0;
+
+[[nodiscard]]
+std::pair<std::string, bool> getLanguageByExtension(const std::map<std::string, std::string>& languages, const std::string&& extension);
+
+[[nodiscard]]
+int getLinesInFile(std::string pathToFile);
+
+void initializeLanguages(std::map<std::string, std::string>& languages);
+
+void addStatistics(const File TYPE);
+
+void fileAnalysisStatistics();
+
+void filesLanguageStatistics(const std::map<int, std::string>& sortedByLines, const int lineAmountInFiles);
+
+int main(int argc, char *argv[])
+{
+    std::cout << '\n' << '\n';
+    std::cout << "Don't delete, rename or move files in the current directory."
+              << '\n' << "Otherwise program can crash.";
+    std::cout << '\n' << '\n';
+
+    std::cout << "Input the number of files to ignore.\n";
+
+    int numberIgnoreFiles;
+    std::cin >> numberIgnoreFiles;
+
+    if (numberIgnoreFiles != 0)
+    {
+        std::cout << "Input the path to files, which must be ignored.\n";
+    }
+    std::set<std::experimental::filesystem::path> ignoreFiles;
+
+    for (int i = 0; i < numberIgnoreFiles; ++i)
+    {
+        std::string temp;
+        std::cin >> temp;
+
+        std::experimental::filesystem::path dir{temp};
+        if (!std::experimental::filesystem::exists(temp))
+        {
+            std::cout << "The path " << dir << " is invalid.\n";
+            continue;
+        }
+
+        dir = std::experimental::filesystem::canonical(dir, std::experimental::filesystem::path{});
+
+        auto it = ignoreFiles.find(dir);
+        if (it != ignoreFiles.end())
+        {
+            std::cout << "You have already inputed this file.\n";
+            continue;
+        }
+        else
+        {
+            ignoreFiles.insert(dir);
+        }
+    }
+
+    if (ignoreFiles.size() != 0)
+    {
+        std::cout << "The following files will be ignored :" << '\n';
+        for (const auto& file : ignoreFiles)
+        {
+            std::cout << file << '\n';
+        }
+    }
+
+    std::string scriptName = argv[0];
+
+
+    std::map<std::string, std::string> languages = {};    // find according files
+    std::map<std::string, int> infoByLanguage = {};                  //  have an information about how many lines each language have
+    std::map<int, std::string> sortedByLines = {};                   // the previous one, but the first field is the amount of lines.
+
+    initializeLanguages(languages);
+
+    std::experimental::filesystem::path directory = std::experimental::filesystem::current_path();
+
+    bool atLeastOneFile = false;
+    int lines_amount = 0;
+    for (const auto& file : std::experimental::filesystem::recursive_directory_iterator{directory})
+    {
+
+        if (std::experimental::filesystem::is_directory(status(file)))
+        {
+            continue;
+        }
+
+        // Ignore the script itself.
+        if (scriptName == std::string(file.path().c_str()))
+        {
+            continue;
+        }
+
+        auto it = ignoreFiles.find(std::string(file.path().c_str()));
+        if (it != ignoreFiles.end())
+        {
+            addStatistics(File::IGNORE);
+            continue;
+        }
+
+        auto [language, isKnownFileExtension] = getLanguageByExtension(languages,
+              std::string(file.path().extension().c_str()));
+        if (!isKnownFileExtension)
+        {
+            addStatistics(File::UNKNOWN);
+            continue;
+        }
+
+        if (std::experimental::filesystem::is_regular_file(status(file)))
+        {
+            addStatistics(File::ADD);
+            atLeastOneFile = true;
+        }
+
+        int currentLines = getLinesInFile( file.path().c_str());
+
+        infoByLanguage[language] += currentLines;
+
+        lines_amount += currentLines;
+
+    }
+
+    for (auto it = infoByLanguage.begin(); it != infoByLanguage.end(); ++it)
+    {
+        sortedByLines[it->second] = it->first;
+    }
+
+    fileAnalysisStatistics();
+    if (atLeastOneFile)
+    {
+        filesLanguageStatistics(sortedByLines, lines_amount);
+    }
+
+    return 0;
+}
+
+
+//  It's guaranteed that file exists.
+//
+[[nodiscard]]
+int getLinesInFile(std::string pathToFile)
+{
+    static constexpr int MAX_EXPECTED_LENGTH = 100;
+    static std::string temp;
+
+    static bool initialized = false;
+    if (!initialized)
+    {
+        temp.reserve(MAX_EXPECTED_LENGTH);
+    }
+    initialized = true;
+
+
+    int lines = 0;
+
+    std::ifstream file;
+    file.open(pathToFile);
+
+    while (getline(file, temp))
+    {
+        ++lines;
+    }
+
+    file.close();
+
+    return lines;
+}
+
+
+//
+//
+void initializeLanguages(std::map<std::string, std::string>& languages)
+{
+    languages[".as"]            = "ActionScript";
+    languages[".adb"]           = "Ada";
+    languages["ads"]            = "Ada";
+    languages[".asm"]           = "Assembler";
+    languages[".masm"]          = "Assembler";
+    languages[".nasm"]          = "Assembler";
+    languages[".bas"]           = "Basic";
+    languages[".c"]             = "C";
+    languages[".h"]             = "C";
+    languages[".cpp"]           = "C++";
+    languages[".cxx"]           = "C++";
+    languages[".c++"]           = "C++";
+    languages[".cc"]            = "C++";
+    languages[".hpp"]           = "C++";
+    languages[".hxx"]           = "C++";
+    languages[".h++"]           = "C++";
+    languages[".hh"]            = "C++";
+    languages[".inl"]           = "C++";
+    languages[".cs"]            = "C#";
+    languages[".css"]           = "CSS";
+    languages[".cg"]            = "Cg";
+    languages[".cbl"]           = "Cobol";
+    languages[".cob"]           = "Cobol";
+    languages[".cpy"]           = "Cobol";
+    languages[".coffee"]        = "CoffeeScript";
+    languages[".litcoffee"]     = "CoffeeScript";
+    languages[".v"]             = "Coq";
+    languages[".cu"]            = "CUDA";
+    languages[".cuh"]           = "CUDA";
+    languages[".cuda"]          = "CUDA";
+    languages[".d"]             = "D";
+    languages[".dart"]          = "Dart";
+    languages[".es"]            = "ECMAScript";
+    languages[".erl"]           = "Erlang";
+    languages[".hrl"]           = "Erlang";
+    languages[".fs"]            = "F#";
+    languages[".fsi"]           = "F#";
+    languages[".fsx"]           = "F#";
+    languages["fsscript"]       = "F#";
+    languages[".f"]             = "Fortran";
+    languages[".for"]           = "Fortran";
+    languages[".f90"]           = "Fortran";
+    languages[".go"]            = "Go";
+    languages[".glsl"]          = "GLSL";
+    languages[".groovy"]        = "Groovy";
+    languages[".hs"]            = "Haskell";
+    languages[".lhs"]           = "Haskell";
+    languages[".hlsl"]          = "HLSL";
+    languages[".html"]          = "HTML";
+    languages[".java"]          = "Java";
+    languages[".js"]            = "JavaScript";
+    languages[".mjs"]           = "JavaScript";
+    languages[".json"]          = "JSON";
+    languages[".kt"]            = "Kotlin";
+    languages[".kts"]           = "Kotlin";
+    languages[".tex"]           = "LaTeX";
+    languages[".lsp"]           = "Lisp";
+    languages[".lua"]           = "Lua";
+    languages[".m"]             = "Objective-C";
+    languages[".mm"]            = "Objective-C";
+    languages[".ml"]            = "OCaml";
+    languages[".mli"]           = "OCaml";
+    languages[".cl"]            = "OpenCL";
+    languages[".pp"]            = "Pascal";
+    languages[".pas"]           = "Pascal";
+    languages[".pl"]            = "Perl";
+    languages[".pm"]            = "Perl";
+    languages[".t"]             = "Perl";
+    languages[".pod"]           = "Perl";
+    languages[".p6"]            = "Perl 6";
+    languages[".pl6"]           = "Perl 6";
+    languages[".pm6"]           = "Perl 6";
+    languages[".php"]           = "PHP";
+    languages[".phtml"]         = "PHP";
+    languages[".php3"]          = "PHP";
+    languages[".php4"]          = "PHP";
+    languages[".php5"]          = "PHP";
+    languages[".php7"]          = "PHP";
+    languages[".phps"]          = "PHP";
+    languages[".php-s"]         = "PHP";
+    languages[".pht"]           = "PHP";
+    languages[".ps"]            = "PostScript";
+    languages[".py"]            = "Python";
+    languages[".q"]             = "Q";
+    languages[".qs"]            = "Q#";
+    languages[".rb"]            = "Ruby";
+    languages[".rs"]            = "Rust";
+    languages[".rlib"]          = "Rust";
+    languages[".sc"]            = "Scala";
+    languages[".scala"]         = "Scala";
+    languages[".swift"]         = "Swift";
+    languages[".ts"]            = "TypeScript";
+    languages[".tsx"]           = "TypeScript";
+
+    return;
+}
+
+
+//
+//
+[[nodiscard]]
+std::pair<std::string, bool> getLanguageByExtension(const std::map<std::string, std::string>& languages, const std::string&& extension)
+{
+    auto it = languages.find(extension);
+
+    if (it != languages.end())
+    {
+        return std::make_pair(it->second, true);
+    }
+
+    return std::make_pair("", false);
+}
+
+//
+//
+void fileAnalysisStatistics()
+{
+
+    std::cout << "=======================================";
+    std::cout << '\n'
+              << "Directory statistics :"
+              << '\n';
+
+    std::cout << '\t' << "Files in the directory: " << files << '.' << '\n' << '\n';
+    std::cout << '\t' << "Ignored files : " << ignored_files << '.' << '\n' << '\n';
+    std::cout << '\t' << "Unknown files : " << unknown_files << '.' << '\n' << '\n';
+
+    std::cout << "=======================================";
+    std::cout << '\n';
+
+    return;
+}
+
+
+//
+//
+void filesLanguageStatistics(const std::map<int, std::string>& sortedByLines, const int lineAmountInFiles)
+{
+
+    std::cout << "=======================================";
+    std::cout << '\n'
+              << "Language statistics:"
+              << '\n';
+
+    for (const auto& lines : sortedByLines)
+    {
+        if (lines.first == 0)
+        {
+            continue;
+        }
+
+        std::cout << '\t' << lines.second;
+
+        float temp = static_cast<float>(lines.first) / lineAmountInFiles;
+        temp *= 100;
+
+        std::cout << '\t' << '\t' << std::setprecision(4) << temp << '\t' << '\t' << lines.first << '\n';
+    }
+
+    std::cout << "=======================================";
+    std::cout << '\n';
+
+    return;
+}
+
+
+//
+//
+void addStatistics(const File TYPE)
+{
+    ++files;
+    switch (TYPE)
+    {
+        case File::IGNORE :
+        {
+            ++ignored_files;
+            break;
+        }
+
+        case File::UNKNOWN :
+        {
+            ++unknown_files;
+            break;
+        }
+
+        case File::ADD :
+        {
+            break;
+        }
+    }
+
+    return;
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,18 @@
+
+1st script
 https://www.spriters-resource.com/
 This site provides a huge ammount of textures for different old games. But when I download any .png file, each file 
 contains the name of the game in the filename of this file. That's why I wrote script to erase after downloading all images,
 that you need for the development.
+
+
+
+2nd script
+Sometimes people want to know how many lines of code of each programming language they have in their project.
+Run this script in the folder, and choose files to ignore.
+After going for a cup of tea, you will see a statistics for the directory and all it's subdirectories.
+This statistics shows you (how great you are) number of files in the projects, ignored files and unknown files.
+Then you will see which programming laguages you use in your project(this is determined by their extension),
+(C and C++ programmers can become confused , becase ".h" belongs to C)
+percent of lines of code according to the other files, and number of lines of code itself.
+
