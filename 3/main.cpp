279e3505e03bc758929a058462218f5ace70db92
LurkerWithScarf <cursedseraph@mail.ru>
2020-01-10 18:10:46 +0300

    Added 3rd script and refactoring.

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,31 +1,235 @@
 #include <iostream>
 #include <fstream>
 
+#include <cstring>
+
+#include <map>
+
+#include <algorithm>
+
+
+//
+//
+enum class Key : size_t
+                {
+                    // Type of guards in file.
+                    // Mutually exclusive.
+                    PRAGMA = 1 << 0,
+                    INCLUDE_GUARDS = 1 << 1,    //  default
+
+                    FOLDER = 1 << 2,            //  default
+
+                };
+
+
+//
+//
+enum class Return : int
+                    {
+                        // When a user wants to see help page.
+                        HELP = 0,
+                        NORMAL = 0,
+
+                        NO_KEY = 1,
+
+                    };
+
+
+void printHelp();
+
+void setDefault(size_t& key);
+
+[[nodiscard]]
+bool haveExtension(const size_t key, Key extension);
+
+void create(const size_t state, std::string_view name);
+void createFile(std::string_view fileName, bool isGuard = true, bool isFolder = true);
+
+void processKey(size_t& state, std::string_view keys_);
+
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    if (argc == 2)
     {
-       std::cout << "Incorrect number of arguments";
-       return -1;
+        if (strncmp(argv[1], "--help", 6) == 0)
+        {
+            printHelp();
+            return static_cast<int>(Return::HELP);
+        }
     }
 
+    size_t state;
 
-    system(("mkdir " + std::string(argv[1])).data());
+    for (int i = 1; i < argc; ++i)
+    {
+        state = 0u;
 
+        if (argv[i][0] == '-')
+        {
+            if (i + 1 >= argc)
+            {
+                std::cout << "No file name after key. "
+                             "Return.\n";
+
+                return static_cast<int>(Return::NO_KEY);
+            }
+
+            // Doesn't include '-' to keys
+            processKey(state, argv[i] + 1);
+
+            ++i;
+        }
+        else
+        {
+           setDefault(state);
+        }
+
+        create(state, argv[i]);
+    }
+
+    return static_cast<int>(Return::NORMAL);
+}
+
+
+//
+//
+void printHelp()
+{
+    std::cout << "Usage: ";
+    std::cout << "crilf [OPTION] FILENAME ...\n";
+    std::cout << "Example: ";
+    std::cout << "crilf -pf temp -p temp_1 temp_2\n";
+
+    std::cout << '\n';
+
+    std::cout << "Pattern protection:\n";
+    std::cout << "-i Include guards.\n";
+    std::cout << "-p Pragma once.\n";
+
+    std::cout << '\n';
+
+    std::cout << "Miscellaneous:\n";
+    std::cout << "-f create file in the folder with the same name.\n";
+
+    std::cout << '\n';
+
+    std::cout << "If file already exists, delete files content.\n";
+    std::cout << "Report bugs to: cursedseraph@mail.ru\n";
+
+    return;
+}
+
+
+//
+//
+void setDefault(size_t& key)
+{
+    key |= static_cast<size_t>(Key::FOLDER) |
+           static_cast<size_t>(Key::INCLUDE_GUARDS);
+
+    return;
+}
+
+
+//
+//
+[[nodiscard]]
+bool haveExtension(const size_t key, Key extension)
+{
+    return static_cast<size_t>(extension) & key;
+}
+
+
+//  If file exists, delete it's content.
+//
+void createFile(std::string_view fileName, bool isGuard, bool isFolder)
+{
     std::ofstream file;
 
-    file.open(std::string(argv[1]).append("/").append(argv[1]).append(".h"));
+    if (isFolder)
+    {
+        file.open(std::string(fileName).append("/").append(fileName).append(".inl"), std::ios_base::trunc);
+    }
+    else
+    {
+        file.open(std::string(fileName).append(".inl"), std::ios_base::trunc);
+    }
 
-    file << std::string("#ifdef ").append(argv[2]);
-    file << '\n';
-    file << std::string("#define ").append(argv[2]);
+    if (isGuard)
+    {
+        std::string name{fileName};
 
-    file << '\n' << '\n' << '\n';
+        std::for_each
+        (name.begin(), name.end(),
+                [](char& symbol)
+                {
+                    symbol = toupper(symbol);
+                }
+        );
 
-    file << std::string("#endif ").append("//").append(argv[2]);
+        file << std::string("#ifdef ").append(name).append("_INL");
+
+        file << '\n';
+
+        file << std::string("#define ").append(name).append("_INL");
+
+        file << "\n\n\n";
+
+        file << std::string("#endif ").append("//").append(name).append("_INL");
+    }
+    else
+    {
+        file << std::string("#pragma once");
+    }
 
     file.close();
 
-    return 0;
+    return;
+}
+
+
+//
+//
+void create(const size_t state, std::string_view name)
+{
+    if (haveExtension(state, Key::FOLDER))
+    {
+        system(("mkdir " + std::string(name)).data());
+    }
+
+    createFile(name, haveExtension(state, Key::INCLUDE_GUARDS), haveExtension(state, Key::FOLDER));
+
+    return;
+}
+
+
+//
+//
+void processKey(size_t& state, std::string_view keys)
+{
+    static  std::map<const char, Key> programmKeys =
+    {
+            {'p', Key::PRAGMA},
+            {'i', Key::INCLUDE_GUARDS},
+            {'f', Key::FOLDER}
+    };
+
+    for (const auto& key : keys)
+    {
+        auto it = programmKeys.find(key);
+
+        if (it != programmKeys.end())
+        {
+            state |= static_cast<size_t>((*it).second);
+            std::cout << static_cast<size_t>((*it).second) << '\n';
+        }
+    }
+
+    if (haveExtension(state, Key::INCLUDE_GUARDS) && haveExtension(state, Key::PRAGMA))
+    {
+        setDefault(state);
+    }
+
+    return;
 }
--- ReadMe.md
+++
@@ -1,8 +0,0 @@
-2nd script
-Sometimes people want to know how many lines of code of each programming language they have in their project.
-Run this script in the folder, and choose files to ignore.
-After going for a cup of tea, you will see a statistics for the directory and all it's subdirectories.
-This statistics shows you (how great you are) number of files in the projects, ignored files and unknown files.
-Then you will see which programming laguages you use in your project(this is determined by their extension),
-(C and C++ programmers can become confused , becase ".h" belongs to C)
-percent of lines of code according to the other files, and number of lines of code itself.
