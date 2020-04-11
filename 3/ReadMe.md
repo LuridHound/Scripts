b342b44fe94b2d4f07dccaea81088e547b36a20f
LurkerWithScarf <cursedseraph@mail.ru>
2020-04-11 22:33:11 +0300

    Added functionality to the 3rd script.

---
+++ 3/ReadMe.md
@@ -0,0 +1,12 @@
+##Motivation
+My IDE doesn't support creation of the .inl file, that already have include guards/pragma.
+Run this script to create necessary files.
+For more information run
+``` cpp
+./crilf --help
+```
+
+### Example
+``` cpp
+./crilf -pf temp -p temp_1 temp_2
+```
--- 3/main.cpp
+++ 3/main.cpp
@@ -2,16 +2,19 @@
 #include <fstream>
 
 #include <cstring>
+#include <algorithm>
 
 #include <map>
 
-#include <algorithm>
+#include <filesystem>
 
 
 //
 //
 enum class Key : size_t
                 {
+                    // Keys
+
                     // Type of guards in file.
                     // Mutually exclusive.
                     PRAGMA = 1 << 0,
@@ -19,70 +22,107 @@ enum class Key : size_t
 
                     FOLDER = 1 << 2,            //  default
 
+                    // Files
+                 
+                    CPP = 1 << 3,
+                    H = 1 << 4,
+                    INL = 1 << 5,
+
+                    // Modes
+                 
+                    EMPTY_MODE = 0,
+                    DEFAULT_MODE = Key::INCLUDE_GUARDS | Key::FOLDER | Key::INL,
+
                 };
 
 
 //
 //
-enum class Return : int
+enum class Return
                     {
                         // When a user wants to see help page.
                         HELP = 0,
                         NORMAL = 0,
 
                         NO_KEY = 1,
-
                     };
 
+enum class Error
+                {
+                    OK = 0,
+                    CANNOT_OPEN_FILE = 1,
+                    FILE_EXISTS = 2
+                };
+
+
+//  For files, that you can forget to close.
+//
+template<class T>
+class SafeFile
+{
+    public :
+
+        SafeFile() = default;
+        ~SafeFile();
+        
+        T& getFile();
+
+    private :
+
+        T file;
+};
+
+
+template<class T>
+static constexpr auto to_integral(T enumValue) -> std::underlying_type_t<T>;
+
+void toUpper(std::string& str);
 
 void printHelp();
 
-void setDefault(size_t& key);
-
 [[nodiscard]]
-bool haveExtension(const size_t key, Key extension);
+bool haveKey(const Key key, Key extension);
 
-void create(const size_t state, std::string_view name);
-void createFile(std::string_view fileName, bool isGuard = true, bool isFolder = true);
+void create(const Key state, std::string_view name);
+[[nodiscard]]
+Error createFile(std::string_view fileName, std::string_view extension, bool isGuard, bool isFolder, bool isCpp);
 
-void processKey(size_t& state, std::string_view keys_);
+void processKey(Key& state, std::string_view keys_);
+
+void validateKey(Key& state);
 
 
 int main(int argc, char* argv[])
 {
     if (argc == 2)
     {
-        if (strncmp(argv[1], "--help", 6) == 0)
+        if (std::strncmp(argv[1], "--help", 6) == 0)
         {
             printHelp();
             return static_cast<int>(Return::HELP);
         }
     }
 
-    size_t state;
+    Key state;
 
     for (int i = 1; i < argc; ++i)
     {
-        state = 0u;
+        state = Key::EMPTY_MODE;
 
-        if (argv[i][0] == '-')
+        while (argv[i][0] == '-')
         {
-            if (i + 1 >= argc)
-            {
-                std::cout << "No file name after key. "
-                             "Return.\n";
-
-                return static_cast<int>(Return::NO_KEY);
-            }
-
             // Doesn't include '-' to keys
             processKey(state, argv[i] + 1);
 
             ++i;
         }
-        else
+
+        validateKey(state);
+
+        // No more strings from the command line.
+        if (i >= argc)
         {
-           setDefault(state);
+           break;
         }
 
         create(state, argv[i]);
@@ -92,144 +132,231 @@ int main(int argc, char* argv[])
 }
 
 
-//
-//
-void printHelp()
+template<class T>
+SafeFile<T>::~SafeFile()
 {
-    std::cout << "Usage: ";
-    std::cout << "crilf [OPTION] FILENAME ...\n";
-    std::cout << "Example: ";
-    std::cout << "crilf -pf temp -p temp_1 temp_2\n";
+    if (file.is_open())
+    {
+        file.close();
+    }
+};
 
-    std::cout << '\n';
 
-    std::cout << "Pattern protection:\n";
-    std::cout << "-i Include guards.\n";
-    std::cout << "-p Pragma once.\n";
+template<class T>
+T& SafeFile<T>::getFile()
+{
+    return file;
+}
 
-    std::cout << '\n';
 
-    std::cout << "Miscellaneous:\n";
-    std::cout << "-f create file in the folder with the same name.\n";
-
-    std::cout << '\n';
-
-    std::cout << "If file already exists, delete files content.\n";
-    std::cout << "Report bugs to: cursedseraph@mail.ru\n";
-
-    return;
+//  Convert enum value to it's underlying type.
+//
+template<class T>
+static constexpr auto to_integral(T enumValue) -> std::underlying_type_t<T>
+{
+    return static_cast<std::underlying_type_t<T>>(enumValue);
 }
 
 
 //
 //
-void setDefault(size_t& key)
+void toUpper(std::string& str)
 {
-    key |= static_cast<size_t>(Key::FOLDER) |
-           static_cast<size_t>(Key::INCLUDE_GUARDS);
+    std::for_each
+            (str.begin(), str.end(),
+             [](char& symbol)
+             {
+                 symbol = toupper(symbol);
+             }
+            );
+}
 
-    return;
+
+//
+//
+void printHelp()
+{
+    std::cout << "Usage: "
+              << "crilf [OPTIONS] FILENAME ...\n"
+              << "Example: "
+              << "crilf -pf temp -p temp_1 temp_2\n"
+
+              << '\n'
+
+              << "Pattern protection:\n"
+              << "-i Include guards.\n"
+              << "-p Pragma once.\n"
+
+              << '\n'
+
+              << "Miscellaneous:\n"
+              << "-f create file in the folder with the same name.\n"
+
+              << "Files:\n"
+              << "-C create .cpp file.\n"
+              << "-H create .h file.\n"
+              << "-I create .inl file.\n"
+
+              << '\n'
+
+              << "Do nothing, if file exists.\n"
+              << "Report bugs to: cursedseraph@mail.ru\n";
 }
 
 
 //
 //
 [[nodiscard]]
-bool haveExtension(const size_t key, Key extension)
+bool haveKey(const Key key, Key extension)
 {
-    return static_cast<size_t>(extension) & key;
+    return to_integral(extension) & to_integral(key);
 }
 
 
 //  If file exists, delete it's content.
-//
-void createFile(std::string_view fileName, bool isGuard, bool isFolder)
+//  Supposed, that extension is without dot.
+[[nodiscard]]
+Error createFile(std::string_view fileName, std::string_view extension, bool isGuard, bool isFolder, bool isCpp)
 {
-    std::ofstream file;
+    SafeFile<std::ofstream> file;
+    
+    if (std::string currentFileNameWithPath; true)
+    {
+        if (isFolder)
+        {
+            currentFileNameWithPath = std::string(fileName).append("/").append(fileName).append(".").append(extension);
+        }
+        else
+        {
+            currentFileNameWithPath = std::string(fileName).append(".").append(extension);
+        }
+        
+        if (std::filesystem::exists(currentFileNameWithPath))
+        {
+            return Error::FILE_EXISTS;
+        }
+        
+        file.getFile().open(currentFileNameWithPath);
+    }
 
-    if (isFolder)
+    if (!file.getFile().is_open())
     {
-        file.open(std::string(fileName).append("/").append(fileName).append(".inl"), std::ios_base::trunc);
+       return Error::CANNOT_OPEN_FILE;
     }
-    else
+
+    std::string fileNameString{fileName};
+    std::string extensionString{extension};
+
+    if (isCpp)
     {
-        file.open(std::string(fileName).append(".inl"), std::ios_base::trunc);
+        return Error::OK;
     }
+
+    toUpper(fileNameString);
+    toUpper(extensionString);
 
     if (isGuard)
     {
-        std::string name{fileName};
 
-        std::for_each
-        (name.begin(), name.end(),
-                [](char& symbol)
-                {
-                    symbol = toupper(symbol);
-                }
-        );
+        file.getFile() << std::string("#ifdef ").append(fileNameString).append("_").append(extensionString)
 
-        file << std::string("#ifdef ").append(name).append("_INL");
+                       << '\n'
 
-        file << '\n';
+                       << std::string("#define ").append(fileNameString).append("_").append(extensionString)
 
-        file << std::string("#define ").append(name).append("_INL");
+                       << "\n\n\n"
 
-        file << "\n\n\n";
-
-        file << std::string("#endif ").append("//").append(name).append("_INL");
+                       << std::string("#endif ").append("//").append(fileNameString).append("_").append(extensionString);
     }
     else
     {
-        file << std::string("#pragma once");
+        file.getFile() << "#pragma once";
     }
 
-    file.close();
-
-    return;
+    return Error::OK;
 }
 
 
 //
 //
-void create(const size_t state, std::string_view name)
+void create(const Key state, std::string_view name)
 {
-    if (haveExtension(state, Key::FOLDER))
+    bool includeGuards = haveKey(state, Key::INCLUDE_GUARDS);
+    bool isFolder = haveKey(state, Key::FOLDER);
+
+    if (isFolder)
     {
         system(("mkdir " + std::string(name)).data());
     }
 
-    createFile(name, haveExtension(state, Key::INCLUDE_GUARDS), haveExtension(state, Key::FOLDER));
+    static constexpr std::array<std::pair<std::string_view, Key>, 3> extensionsKeys = {std::make_pair("cpp", Key::CPP),
+                                                                                       std::make_pair("h", Key::H),
+                                                                                       std::make_pair("inl", Key::INL)};
 
-    return;
+    static constexpr std::array<std::string_view, 3> errorsLogs = {"OK", "Cannot create file", "File already exists"};
+
+    for (auto&& key = extensionsKeys.begin(); key != extensionsKeys.end(); ++key)
+    {
+        Error error = Error::OK;
+
+        if (haveKey(state, key->second))
+        {
+            error = createFile(name, key->first, includeGuards, isFolder, key->second == Key::CPP);
+        }
+
+        if (error != Error::OK)
+        {
+           std::cout << errorsLogs[to_integral(error)] << ':' << ' ' << name << '.' << key->first << '\n';
+        }
+    }
 }
 
 
 //
 //
-void processKey(size_t& state, std::string_view keys)
+void processKey(Key& state, std::string_view keys)
 {
-    static  std::map<const char, Key> programmKeys =
+    static  std::map<const char, Key> programKeys =
     {
             {'p', Key::PRAGMA},
             {'i', Key::INCLUDE_GUARDS},
-            {'f', Key::FOLDER}
+            {'f', Key::FOLDER},
+            {'H', Key::H},
+            {'C', Key::CPP},
+            {'I', Key::INL}
     };
-
+    
     for (const auto& key : keys)
     {
-        auto it = programmKeys.find(key);
+        auto it = programKeys.find(key);
 
-        if (it != programmKeys.end())
+        if (it != programKeys.end())
         {
-            state |= static_cast<size_t>((*it).second);
-            std::cout << static_cast<size_t>((*it).second) << '\n';
+            state = static_cast<Key>(to_integral(state) | to_integral((*it).second));
         }
     }
+}
 
-    if (haveExtension(state, Key::INCLUDE_GUARDS) && haveExtension(state, Key::PRAGMA))
-    {
-        setDefault(state);
+
+//
+//
+void validateKey(Key& state) 
+{
+    //  Pragma has a lower priority to be chosen.
+    if (haveKey(state, Key::INCLUDE_GUARDS) && haveKey(state, Key::PRAGMA)) {
+        state = static_cast<Key>(to_integral(state) ^ to_integral(Key::PRAGMA));
     }
 
-    return;
+    bool noState = state == Key::EMPTY_MODE;
+    bool noExtensions = !(haveKey(state, Key::CPP) || haveKey(state, Key::H) ||
+                          haveKey(state, Key::INL));
+
+    bool noGuard = !(haveKey(state, Key::PRAGMA) || haveKey(state, Key::INCLUDE_GUARDS)) &&
+                   !haveKey(state, Key::CPP);
+
+    // Just add necessary flags for work.
+    if (noState || noExtensions || noGuard) 
+    {
+        state = static_cast<Key>(to_integral(state) | to_integral(Key::DEFAULT_MODE));
+    }
 }
